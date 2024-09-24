27f2d62483fc2ee8031fda38dcd7aa34a8dfde4c
HadiElnemr <hadi.elnemr@gmail.com>
2024-09-24 18:14:46 +0200

    Remove files

--- Mini bike/3. Small bicycle standard version HAL library version_GMR encoder_2022.10.18/3.╨í╨═╫╘╨╨│╡▒Ω╫╝░µHAL┐Γ░µ▒╛_GMR▒α┬δ╞≈_2022.10.18/Debug/WHEELTEC/DataScope_DP.d
+++
@@ -1,3 +0,0 @@
-WHEELTEC/DataScope_DP.o: ../WHEELTEC/DataScope_DP.c \
- C:/Users/Administrator/Desktop/WHEELTEC/WHEELTEC/Inc/DataScope_DP.h
-C:/Users/Administrator/Desktop/WHEELTEC/WHEELTEC/Inc/DataScope_DP.h:
