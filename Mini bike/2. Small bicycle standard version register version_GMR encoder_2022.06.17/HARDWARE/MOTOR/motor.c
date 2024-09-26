daaaf89a5da18bb3595e7ae2e7e88f61a8d0d5b4
HadiElnemr <hadi.elnemr@gmail.com>
2024-09-26 17:16:21 +0200

    Add translation for comments in codes

--- Mini bike/2. Small bicycle standard version register version_GMR encoder_2022.06.17/HARDWARE/MOTOR/motor.c
+++ Mini bike/2. Small bicycle standard version register version_GMR encoder_2022.06.17/HARDWARE/MOTOR/motor.c
@@ -5,47 +5,47 @@
 **************************************************************************/
 void MiniBalance_Motor_Init(void)
 {
-	RCC->APB2ENR|=1<<2;        //PORTA时钟使能   
-  RCC->APB2ENR|=1<<3;        //PORTB时钟使能   
+	RCC->APB2ENR|=1<<2;        //PORTA时钟使能  //PORTA clock enable
+  RCC->APB2ENR|=1<<3;        //PORTB时钟使能    //PORTB clock enable
 
 	GPIOA->CRH&=0XFFF00FFF;   //PA11、12
-	GPIOA->CRH|=0X0002B000;   //PA11复用推挽输出(用于舵机)；PA12推挽输出(使能飞轮引脚)
-	
-	GPIOB->CRL&=0X00FFFFFF;   
-	GPIOB->CRL|=0XBB000000;   //PB6、7复用推挽输出，产生PWM(用于后轮电机驱动)
-														
-	GPIOB->CRH&=0XFFFFFF00;   
-	GPIOB->CRH|=0X000000BB;   //PB8、9复用推挽输出，产生PWM(用于飞轮电机驱动)
+	GPIOA->CRH|=0X0002B000;   //PA11复用推挽输出(用于舵机)；PA12推挽输出(使能飞轮引脚)  //PA11 multiplexed push-pull output (for steering gear); PA12 push-pull output (enable flywheel pin)
+
+	GPIOB->CRL&=0X00FFFFFF;
+	GPIOB->CRL|=0XBB000000;   //PB6、7复用推挽输出，产生PWM(用于后轮电机驱动)    //PB6, 7 multiplexed push-pull output, generating PWM (for rear wheel motor drive)
+
+	GPIOB->CRH&=0XFFFFFF00;
+	GPIOB->CRH|=0X000000BB;   //PB8、9复用推挽输出，产生PWM(用于飞轮电机驱动)    //PB8, 9 multiplexed push-pull output, generating PWM (for flywheel motor drive)
 }
 void MiniBalance_PWM_Init(u16 arr,u16 psc)//TIM4 12  TIM
-{		 					 
+{
 
-	MiniBalance_Motor_Init();  //初始化电机控制所需IO
-	RCC->APB1ENR|=1<<2;       	//使能TIM4时钟    
-	TIM4->ARR=arr;             //设定计数器自动重装值 
-	TIM4->PSC=psc;             //预分频器不分频
-	
-	TIM4->CCMR1|=6<<4;         //CH1 PWM1模式	
-	TIM4->CCMR1|=6<<12;        //CH2 PWM1模式	
-	TIM4->CCMR2|=6<<4;         //CH3 PWM1模式
-	TIM4->CCMR2|=6<<12;        //CH4 PWM1模式	
-		
-	TIM4->CCMR1|=1<<3;         //CH1预装载使能	
-	TIM4->CCMR1|=1<<11;        //CH2预装载使能	 
-	TIM4->CCMR2|=1<<3;         //CH3预装载使能	  
-	TIM4->CCMR2|=1<<11;        //CH4预装载使能	 
-	  
-	TIM4->CCER|=1<<0;          //CH1输出使能	
-	TIM4->CCER|=1<<4;          //CH2输出使能	   
-	TIM4->CCER|=1<<8;          //CH3输出使能	   
-	TIM4->CCER|=1<<12;         //CH4输出使能	
-	
-	TIM4->CR1=0x0080;          	 //ARPE使能 
-	TIM4->CR1|=0x01;           //使能定时器4 			
+	MiniBalance_Motor_Init();  //初始化电机控制所需IO    //Initialise the IO required for motor control
+	RCC->APB1ENR|=1<<2;       	//使能TIM4时钟         //Enable TIM4 clock
+	TIM4->ARR=arr;             //设定计数器自动重装值    //Set the counter auto-reload value
+	TIM4->PSC=psc;             //预分频器不分频         //The pre-scaler is not divided
 
-	//防止飞轮转动
+	TIM4->CCMR1|=6<<4;         //CH1 PWM1模式 	   	    //CH1 PWM1 mode
+	TIM4->CCMR1|=6<<12;        //CH2 PWM1模式 		    //CH2 PWM1 mode
+	TIM4->CCMR2|=6<<4;         //CH3 PWM1模式		    //CH3 PWM1 mode
+	TIM4->CCMR2|=6<<12;        //CH4 PWM1模式			//CH4 PWM1 mode
+
+	TIM4->CCMR1|=1<<3;         //CH1预装载使能		     //CH1 pre-load enable
+	TIM4->CCMR1|=1<<11;        //CH2预装载使能			 //CH2 pre-load enable
+	TIM4->CCMR2|=1<<3;         //CH3预装载使能			 //CH3 pre-load enable
+	TIM4->CCMR2|=1<<11;        //CH4预装载使能			 //CH4 pre-load enable
+
+	TIM4->CCER|=1<<0;          //CH1输出使能			 //CH1 output enable
+	TIM4->CCER|=1<<4;          //CH2输出使能			 //CH2 output enable
+	TIM4->CCER|=1<<8;          //CH3输出使能			 //CH3 output enable
+	TIM4->CCER|=1<<12;         //CH4输出使能			 //CH4 output enable
+
+	TIM4->CR1=0x0080;          	 //ARPE使能				 //ARPE enable
+	TIM4->CR1|=0x01;           //使能定时器4		      //Enable Timer 4
+
+	//防止飞轮转动	//Prevent flywheel rotation
 	AIN1 = 0;
 	AIN2 = 0;
 	AEN = 0;
-} 
+}
 
