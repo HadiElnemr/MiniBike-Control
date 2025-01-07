c65db76660cd8051c41fcc2df08d5e18362ae462
HadiElnemr <hadi.elnemr@gmail.com>
2025-01-07 15:27:31 +0100

    fix some comments translations

--- MiniBike/Bike Documentation and some codes/zipped codes/3. Small bicycle standard version HAL library version_GMR encoder_2022.10 .18/3. Small bicycle standard version HAL library version_GMR encoder_2022.10 .18/BSPcode/Inc/iic.h
+++ MiniBike/Bike Documentation and some codes/zipped codes/3. Small bicycle standard version HAL library version_GMR encoder_2022.10 .18/3. Small bicycle standard version HAL library version_GMR encoder_2022.10 .18/BSPcode/Inc/iic.h
@@ -24,22 +24,22 @@
 void IIC_Init(void);                //��ʼ��IIC��IO�� Initialize IIC IO port
 int IIC_Start(void);				//����IIC��ʼ�ź� Start IIC
 void IIC_Stop(void);	  			//����IICֹͣ�ź� Stop IIC
-void IIC_Send_Byte(u8 txd);			//IIC����һ���ֽ� Send one byte of data via I2C
-u8 IIC_Read_Byte(unsigned char ack);//IIC��ȡһ���ֽ� Read one byte of data via I2C
+void IIC_Send_Byte(uint8_t txd);			//IIC����һ���ֽ� Send one byte of data via I2C
+uint8_t IIC_Read_Byte(unsigned char ack);//IIC��ȡһ���ֽ� Read one byte of data via I2C
 int IIC_Wait_Ack(void); 			//IIC�ȴ�ACK�ź� Wait for ACK signal
 void IIC_Ack(void);					//IIC����ACK�ź� Send ACK signal
 void IIC_NAck(void);				//IIC������ACK�ź�  Send NACK signal
 
-void IIC_Write_One_Byte(u8 daddr,u8 addr,u8 data);
-u8 IIC_Read_One_Byte(u8 daddr,u8 addr);
+void IIC_Write_One_Byte(uint8_t daddr,uint8_t addr,uint8_t data);
+uint8_t IIC_Read_One_Byte(uint8_t daddr,uint8_t addr);
 unsigned char I2C_Readkey(unsigned char I2C_Addr);
 
 unsigned char I2C_ReadOneByte(unsigned char I2C_Addr,unsigned char addr);
 unsigned char IICwriteByte(unsigned char dev, unsigned char reg, unsigned char data);
-u8 IICwriteBytes(u8 dev, u8 reg, u8 length, u8* data);
-u8 IICwriteBits(u8 dev,u8 reg,u8 bitStart,u8 length,u8 data);
-u8 IICwriteBit(u8 dev,u8 reg,u8 bitNum,u8 data);
-u8 IICreadBytes(u8 dev, u8 reg, u8 length, u8 *data);
+uint8_t IICwriteBytes(uint8_t dev, uint8_t reg, uint8_t length, uint8_t* data);
+uint8_t IICwriteBits(uint8_t dev,uint8_t reg,uint8_t bitStart,uint8_t length,uint8_t data);
+uint8_t IICwriteBit(uint8_t dev,uint8_t reg,uint8_t bitNum,uint8_t data);
+uint8_t IICreadBytes(uint8_t dev, uint8_t reg, uint8_t length, uint8_t *data);
 
 int i2cWrite(uint8_t addr, uint8_t reg, uint8_t len, uint8_t *data);
 int i2cRead(uint8_t addr, uint8_t reg, uint8_t len, uint8_t *buf);
--- MiniBike/Bike Documentation and some codes/zipped codes/3. Small bicycle standard version HAL library version_GMR encoder_2022.10 .18/3. Small bicycle standard version HAL library version_GMR encoder_2022.10 .18/BSPcode/iic.c
+++ MiniBike/Bike Documentation and some codes/zipped codes/3. Small bicycle standard version HAL library version_GMR encoder_2022.10 .18/3. Small bicycle standard version HAL library version_GMR encoder_2022.10 .18/BSPcode/iic.c
@@ -30,7 +30,7 @@ int IIC_Start(void)
 /**************************ʵ�ֺ���********************************************
 *����ԭ��:		void IIC_Stop(void)
 *��������:	    //����IICֹͣ�ź�
-*******************************************************************************/
+****************************************************************************/
 void IIC_Stop(void)
 {
 	SDA_OUT();//sda�����
@@ -42,15 +42,15 @@ void IIC_Stop(void)
  	HAL_Delay_us(1);
 }
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		u8 IIC_Wait_Ack(void)
-*��������:	    �ȴ�Ӧ���źŵ���
-//����ֵ��1������Ӧ��ʧ��
-//        0������Ӧ��ɹ�
-*******************************************************************************/
+/**************************Function Implementation**************************
+* Function Prototype: uint8_t IIC_Wait_Ack(void)
+* Function Description: Waits for an ACK (Acknowledge) signal.
+* Return Value: 1 - If ACK failed
+*               0 - If ACK succeeded
+***************************************************************************/
 int IIC_Wait_Ack(void)
 {
-	u8 ucErrTime=0;
+	uint8_t ucErrTime=0;
 	SDA_IN();      //SDA����Ϊ����
 	IIC_SDA_SET;
 	HAL_Delay_us(1);
@@ -70,10 +70,10 @@ int IIC_Wait_Ack(void)
 	return 1;
 }
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		void IIC_Ack(void)
-*��������:	    ����ACKӦ��
-*******************************************************************************/
+/**************************Function Implementation***************************
+* Function Prototype: void IIC_Ack(void)
+* Function Description: Sends an ACK (Acknowledge) signal.
+*****************************************************************************/
 void IIC_Ack(void)
 {
 	IIC_SCL_RESET;
@@ -85,10 +85,10 @@ void IIC_Ack(void)
 	IIC_SCL_RESET;
 }
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		void IIC_NAck(void)
-*��������:	    ����NACKӦ��
-*******************************************************************************/
+/**************************Function Implementation***************************
+* Function Prototype: void IIC_NAck(void)
+* Function Description: Sends a NACK (Not Acknowledge) signal.
+*****************************************************************************/
 void IIC_NAck(void)
 {
 	IIC_SCL_RESET;
@@ -99,13 +99,14 @@ void IIC_NAck(void)
 	HAL_Delay_us(1);
 	IIC_SCL_RESET;
 }
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		void IIC_Send_Byte(u8 txd)
-*��������:	    IIC����һ���ֽ�
-*******************************************************************************/
-void IIC_Send_Byte(u8 txd)
+
+/**************************Function Implementation********************************************
+* Function Prototype: void IIC_Send_Byte(uint8_t txd)
+* Function Description: Sends one byte of data via IIC.
+***********************************************************************************************/
+void IIC_Send_Byte(uint8_t txd)
 {
-    u8 t;
+    uint8_t t;
 	SDA_OUT();
 	IIC_SCL_RESET;//����ʱ�ӿ�ʼ���ݴ���
     for(t=0;t<8;t++)
@@ -121,10 +122,10 @@ void IIC_Send_Byte(u8 txd)
     }
 }
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		bool i2cWrite(uint8_t addr, uint8_t reg, uint8_t data)
-*��������:
-*******************************************************************************/
+/**************************Function Implementation********************************************
+* Function Prototype: bool i2cWrite(uint8_t addr, uint8_t reg, uint8_t data)
+* Function Description: Writes a single byte of data to a specified register of the target device.
+***********************************************************************************************/
 int i2cWrite(uint8_t addr, uint8_t reg, uint8_t len, uint8_t *data)
 {
 		int i;
@@ -147,10 +148,11 @@ int i2cWrite(uint8_t addr, uint8_t reg, uint8_t len, uint8_t *data)
     IIC_Stop();
     return 0;
 }
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		bool i2cWrite(uint8_t addr, uint8_t reg, uint8_t data)
-*��������:
-*******************************************************************************/
+
+/**************************Function Implementation********************************************
+* Function Prototype: bool i2cWrite(uint8_t addr, uint8_t reg, uint8_t data)
+* Function Description: Sends a single byte of data to a specified register of the target device.
+***********************************************************************************************/
 int i2cRead(uint8_t addr, uint8_t reg, uint8_t len, uint8_t *buf)
 {
     if (!IIC_Start())
@@ -178,11 +180,11 @@ int i2cRead(uint8_t addr, uint8_t reg, uint8_t len, uint8_t *buf)
 }
 
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		u8 IIC_Read_Byte(unsigned char ack)
-*��������:	    //��1���ֽڣ�ack=1ʱ������ACK��ack=0������nACK
-*******************************************************************************/
-u8 IIC_Read_Byte(unsigned char ack)
+/**************************Function Implementation********************************************
+* Function Prototype: uint8_t IIC_Read_Byte(unsigned char ack)
+* Function Description: Reads 1 byte. If ack=1, an ACK signal is sent; if ack=0, a NACK signal is sent.
+***********************************************************************************************/
+uint8_t IIC_Read_Byte(unsigned char ack)
 {
 	unsigned char i,receive=0;
 	SDA_IN();//SDA����Ϊ����
@@ -202,13 +204,15 @@ u8 IIC_Read_Byte(unsigned char ack)
     return receive;
 }
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		unsigned char I2C_ReadOneByte(unsigned char I2C_Addr,unsigned char addr)
-*��������:	    ��ȡָ���豸 ָ���Ĵ�����һ��ֵ
-����	I2C_Addr  Ŀ���豸��ַ
-		addr	   �Ĵ�����ַ
-����   ��������ֵ
-*******************************************************************************/
+/**************************Function Implementation********************************************
+* Function Prototype: unsigned char I2C_ReadOneByte(unsigned char I2C_Addr, unsigned char addr)
+* Function Description: Read a single byte from a specified register of the target device.
+* Parameters:
+*       I2C_Addr - Target device address
+*       addr     - Register address
+* Return Value:
+*       The value read from the specified register.
+***********************************************************************************************/
 unsigned char I2C_ReadOneByte(unsigned char I2C_Addr,unsigned char addr)
 {
 	unsigned char res=0;
@@ -230,17 +234,19 @@ unsigned char I2C_ReadOneByte(unsigned char I2C_Addr,unsigned char addr)
 }
 
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		u8 IICreadBytes(u8 dev, u8 reg, u8 length, u8 *data)
-*��������:	    ��ȡָ���豸 ָ���Ĵ����� length��ֵ
-����	dev  Ŀ���豸��ַ
-		reg	  �Ĵ�����ַ
-		length Ҫ�����ֽ���
-		*data  ���������ݽ�Ҫ��ŵ�ָ��
-����   ���������ֽ�����
-*******************************************************************************/
-u8 IICreadBytes(u8 dev, u8 reg, u8 length, u8 *data){
-    u8 count = 0;
+/**************************Function Implementation********************************************
+* Function Prototype: uint8_t IICreadBytes(uint8_t dev, uint8_t reg, uint8_t length, uint8_t *data)
+* Function Description: Read a specified number of bytes (`length`) from a specified register of the target device.
+* Parameters:
+*       dev    - Target device address
+*       reg    - Register address
+*       length - Number of bytes to read
+*       *data  - Pointer where the read data will be stored
+* Return Value:
+*       The number of bytes successfully read.
+***********************************************************************************************/
+uint8_t IICreadBytes(uint8_t dev, uint8_t reg, uint8_t length, uint8_t *data){
+    uint8_t count = 0;
 
 	IIC_Start();
 	IIC_Send_Byte(dev);	   //����д����
@@ -260,18 +266,20 @@ u8 IICreadBytes(u8 dev, u8 reg, u8 length, u8 *data){
     return count;
 }
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		u8 IICwriteBytes(u8 dev, u8 reg, u8 length, u8* data)
-*��������:	    ������ֽ�д��ָ���豸 ָ���Ĵ���
-����	dev  Ŀ���豸��ַ
-		reg	  �Ĵ�����ַ
-		length Ҫд���ֽ���
-		*data  ��Ҫд�����ݵ��׵�ַ
-����   �����Ƿ�ɹ�
-*******************************************************************************/
-u8 IICwriteBytes(u8 dev, u8 reg, u8 length, u8* data){
+/**************************Function Implementation********************************************
+* Function Prototype: uint8_t IICwriteBytes(uint8_t dev, uint8_t reg, uint8_t length, uint8_t* data)
+* Function Description: Write multiple bytes to a specified register of the target device.
+* Parameters:
+*       dev    - Target device address
+*       reg    - Register address
+*       length - Number of bytes to write
+*       *data  - Pointer to the data to be written
+* Return Value:
+*       Indicates whether the operation was successful.
+***********************************************************************************************/
+uint8_t IICwriteBytes(uint8_t dev, uint8_t reg, uint8_t length, uint8_t* data){
 
- 	u8 count = 0;
+ 	uint8_t count = 0;
 	IIC_Start();
 	IIC_Send_Byte(dev);	   //����д����
 	IIC_Wait_Ack();
@@ -286,48 +294,55 @@ u8 IICwriteBytes(u8 dev, u8 reg, u8 length, u8* data){
     return 1; //status == 0;
 }
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		u8 IICreadByte(u8 dev, u8 reg, u8 *data)
-*��������:	    ��ȡָ���豸 ָ���Ĵ�����һ��ֵ
-����	dev  Ŀ���豸��ַ
-		reg	   �Ĵ�����ַ
-		*data  ���������ݽ�Ҫ��ŵĵ�ַ
-����   1
-*******************************************************************************/
-u8 IICreadByte(u8 dev, u8 reg, u8 *data){
+/**************************Function Implementation********************************************
+* Function Prototype: uint8_t IICreadByte(uint8_t dev, uint8_t reg, uint8_t *data)
+* Function Description: Read a single byte value from a specified register of the target device.
+* Parameters:
+*       dev   - Target device address
+*       reg   - Register address
+*       *data - Pointer to store the read data
+* Return Value:
+*       1 (indicating success)
+***********************************************************************************************/
+uint8_t IICreadByte(uint8_t dev, uint8_t reg, uint8_t *data){
 	*data=I2C_ReadOneByte(dev, reg);
     return 1;
 }
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		unsigned char IICwriteByte(unsigned char dev, unsigned char reg, unsigned char data)
-*��������:	    д��ָ���豸 ָ���Ĵ���һ���ֽ�
-����	dev  Ŀ���豸��ַ
-		reg	   �Ĵ�����ַ
-		data  ��Ҫд����ֽ�
-����   1
-*******************************************************************************/
+/**************************Function Implementation********************************************
+* Function Prototype: unsigned char IICwriteByte(unsigned char dev, unsigned char reg, unsigned char data)
+* Function Description: Write one byte to a specified register of the target device.
+* Parameters:
+*       dev   - Target device address
+*       reg   - Register address
+*       data  - The byte to be written
+* Return Value:
+*       1 (indicating success)
+***********************************************************************************************/
 unsigned char IICwriteByte(unsigned char dev, unsigned char reg, unsigned char data){
     return IICwriteBytes(dev, reg, 1, &data);
 }
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		u8 IICwriteBits(u8 dev,u8 reg,u8 bitStart,u8 length,u8 data)
-*��������:	    �� �޸� д ָ���豸 ָ���Ĵ���һ���ֽ� �еĶ��λ
-����	dev  Ŀ���豸��ַ
-		reg	   �Ĵ�����ַ
-		bitStart  Ŀ���ֽڵ���ʼλ
-		length   λ����
-		data    ��Ÿı�Ŀ���ֽ�λ��ֵ
-����   �ɹ� Ϊ1
- 		ʧ��Ϊ0
-*******************************************************************************/
-u8 IICwriteBits(u8 dev,u8 reg,u8 bitStart,u8 length,u8 data)
+/**************************Function Implementation********************************************
+* Function Prototype: uint8_t IICwriteBits(uint8_t dev, uint8_t reg, uint8_t bitStart, uint8_t length, uint8_t data)
+* Function Description: Modify multiple bits in one byte of a specified register in the target device.
+* Parameters:
+*       dev       - Target device address
+*       reg       - Register address
+*       bitStart  - Starting position of the bits to be modified in the target byte
+*       length    - Number of bits to be modified
+*       data      - The new value for the specified bits
+* Return Value:
+*       Success   - 1
+*       Failure   - 0
+***********************************************************************************************/
+
+uint8_t IICwriteBits(uint8_t dev,uint8_t reg,uint8_t bitStart,uint8_t length,uint8_t data)
 {
 
-    u8 b;
+    uint8_t b;
     if (IICreadByte(dev, reg, &b) != 0) {
-        u8 mask = (0xFF << (bitStart + 1)) | 0xFF >> ((8 - bitStart) + length - 1);
+        uint8_t mask = (0xFF << (bitStart + 1)) | 0xFF >> ((8 - bitStart) + length - 1);
         data <<= (8 - length);
         data >>= (7 - bitStart);
         b &= mask;
@@ -338,18 +353,21 @@ u8 IICwriteBits(u8 dev,u8 reg,u8 bitStart,u8 length,u8 data)
     }
 }
 
-/**************************ʵ�ֺ���********************************************
-*����ԭ��:		u8 IICwriteBit(u8 dev, u8 reg, u8 bitNum, u8 data)
-*��������:	    �� �޸� д ָ���豸 ָ���Ĵ���һ���ֽ� �е�1��λ
-����	dev  Ŀ���豸��ַ
-		reg	   �Ĵ�����ַ
-		bitNum  Ҫ�޸�Ŀ���ֽڵ�bitNumλ
-		data  Ϊ0 ʱ��Ŀ��λ������0 ���򽫱���λ
-����   �ɹ� Ϊ1
- 		ʧ��Ϊ0
-*******************************************************************************/
-u8 IICwriteBit(u8 dev, u8 reg, u8 bitNum, u8 data){
-    u8 b;
+/**************************Function Implementation********************************************
+* Function Prototype: uint8_t IICwriteBit(uint8_t dev, uint8_t reg, uint8_t bitNum, uint8_t data)
+* Function Description: Modify a specific bit in one byte of a specified register in the target device.
+* Parameters:
+*       dev     - Target device address
+*       reg     - Register address
+*       bitNum  - The bit position to be modified in the target byte
+*       data    - When 0, the target bit will be cleared (set to 0); otherwise, the target bit will be set
+* Return Value:
+*       Success - 1
+*       Failure - 0
+***********************************************************************************************/
+
+uint8_t IICwriteBit(uint8_t dev, uint8_t reg, uint8_t bitNum, uint8_t data){
+    uint8_t b;
     IICreadByte(dev, reg, &b);
     b = (data != 0) ? (b | (1 << bitNum)) : (b & ~(1 << bitNum));
     return IICwriteByte(dev, reg, b);
