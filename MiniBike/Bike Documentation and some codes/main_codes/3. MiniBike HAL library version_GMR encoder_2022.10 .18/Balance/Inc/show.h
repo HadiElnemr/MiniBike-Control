8438fa59d4e7c8eb5249a2fe788b6fa33447a4a1
HadiElnemr <hadi.elnemr@gmail.com>
2025-01-21 15:24:03 +0100

    add externs to debug vars

--- MiniBike/Bike Documentation and some codes/main_codes/3. MiniBike HAL library version_GMR encoder_2022.10 .18/Balance/Inc/show.h
+++ MiniBike/Bike Documentation and some codes/main_codes/3. MiniBike HAL library version_GMR encoder_2022.10 .18/Balance/Inc/show.h
@@ -9,4 +9,19 @@ void oled_show(void);
 void APP_Show(void);
 void DataScope(void);
 extern short test_num;
+
+// extern float SERVO;
+extern float LeftControl;
+extern float RightControl;
+// extern float kp,ki,kd;
+extern float balance_point;
+extern int Voltage;
+extern int Encoder;
+extern int Motor;
+extern int Servo;
+// extern float phi;
+extern u8 Flag_Stop;
+extern u8 Flag_Show;
+extern u8 PID_Send;
+
 #endif
