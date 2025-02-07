15be0c291d4778166a3ca0fd5c41d5476d236b5b
HadiElnemr <hadi.elnemr@gmail.com>
2025-02-07 23:27:29 +0100

    DDMPC feat: add epsilon calculation for given data and refactor codes for data-driven state-feedback

--- DDMPC Codes/bike_model_code_ACC/DD_MinMax_MPC_nominal_with_bike_data.m
+++ DDMPC Codes/bike_model_code_ACC/DD_MinMax_MPC_nominal_with_bike_data.m
@@ -23,21 +23,24 @@ v = 0.634; % m/s or v = 0.634
 a = 0.055; % m (distance between rear wheel and centre of gravity projection)
 w = 0.167; % m (Distance between front and rear wheels and ground contact points)
 lambda =  75/180 * pi; % in rad = 70 degrees  (fork angle)
+% lambda =  90/180 * pi;
 wheel_radius = 0.0375; % m (diameter is around 7.5cm)
 r_tau = wheel_radius * tan(pi/2 - lambda); %(distance between front wheel and intersection of fork with ground)
 l = w; % length
 
 
 A23 = -(v^2*h-a*r_tau*g) * sin(lambda) / (h^2*l);
+% A23 = -(v^2*h-a*r_tau*g) / (h^2*l);
+B2 = - a*v*sin(lambda)/(h*l) ; % Try with negative: yes
 
 % true system matrices
 A_c = [0   1   0;
       g/h  0  A23;
        0   0   0];
 B_c = [0 ; 
-       - a*v*sin(lambda)/(h*l) ; % Try with negative: yes
+       B2;
        1];
-
+% states are phi, phi_dot, delta
 % Discretise
 
 Ts = 0.020; % Sampling time as in bike simulations
@@ -48,7 +51,7 @@ A_s = sysd.A;
 B_s = sysd.B;
 
 % constraint on noise
-epsilon = 10^-6;
+% epsilon = 10^-6;
 
 % input constraint and state constraint
 u_max = 0.01;
@@ -58,14 +61,15 @@ x_hat = [0;0;0];
 % S_x = [10000 0 0; 0 5000 0; 0 0 3000];
 
 S_u = 0.01;
-S_x = [10 0 0; 0 5 0; 0 0 3];
+% S_x = [10 0 0; 0 5 0; 0 0 3];
+S_x = [1.7754 0 0; 0 5 0; 0 0 3];
 % Lean/roll angle max is 40 degrees and motor disconnects after that
 
 
 %% Generate data with additive noise
 
 % length of the initial available data
-T = 15;
+T = 90;
 
 % initial state for data generate
 % x_d0 = [rand(1); -rand(1)];
@@ -78,7 +82,8 @@ x_d0 = [0.001*rand(1); -0.01*rand(1); 0.001*rand(1)]; % Change ()very small
 load('matlab_data.mat');
 % x_g = x_g(100:300);
 % u_g = u_g(100:300);
-
+epsilon = calculate_epsilon(sysd, x_g, u_g);
+% epsilon = 0.4426;
 %% Simulation
 
 % equilibrium point
@@ -100,7 +105,6 @@ x_init = [-0.01;-0.04; 0];
 
 % Number of MPC iterations
 % mpciterations = 300;
-mpciterations = 100;
 
 % set options for the solver
 option = sdpsettings('solver','mosek','verbose',2,'debug',1) % ,'mosek.MSK_DPAR_INTPNT_CO_TOL_REL_GAP', 1e-8
@@ -132,8 +136,8 @@ con_c = [[-H zeros(n,n+m);zeros(n+m,n) zeros(n+m,n+m)]+Pi_tau [zeros(n,n);H;L] z
           [zeros(n,n);H;L]' -H  [MR*L;MQ*H]';
           zeros(n+m,m+2*n) [MR*L;MQ*H] -gamma*eye(n+m,n+m)]<=-10^-12*eye(4*n+2*m);
 con_d = tau(:)>=10^-12;
-con_e = [H L'; L inv(S_u)]>=10^-12*eye(n+m);
-con_f = [H H; H inv(S_x)]>=10^-12*eye(2*n);
+% con_e = [H L'; L inv(S_u)]>=10^-12*eye(n+m);
+% con_f = [H H; H inv(S_x)]>=10^-12*eye(2*n);
 % ?? con_f = [S_x eye(); eye H]>=10^-12*eye(2*n); ??
 
 % initial state  
@@ -148,7 +152,7 @@ L_op = [];
 tau_op = [];
 F_op = [];
 t = [];
-mpciterations = 15;
+mpciterations = 1;
 for ii=1:mpciterations
 
     t_Start = tic;
@@ -157,7 +161,7 @@ for ii=1:mpciterations
     con_b = [1 xmeasure'; xmeasure H]>=10^-7*eye(n+1);
     
     % solve the problem with LMI constraints
-    LMI = [con_b,con_c,con_d,con_e,con_f];
+    LMI = [con_b,con_c,con_d];
 
     % solution
     P = optimizer(LMI,gamma,option,[],{gamma,H,L,tau});
@@ -227,7 +231,7 @@ t = 0:Ts:10;
 u = zeros(size(t));
 
 x0 = [0.0873; 0; 0]; % bike sims
-x0 = [-0.01;-0.04; 0];
+% x0 = [-0.01;-0.04; 0];
 
 y = dlsim(A_feedback,B_s,C,D,u,x0);
 
---
+++ DDMPC Codes/bike_model_code_ACC/calculate_epsilon.m
@@ -0,0 +1,25 @@
+function [epsilon] = calculate_epsilon(sysd, x_g, u_g)
+%calculate_epsilon Given the discretised system, x_g, and u_g collected
+%data,ouput epsilon.
+%  calculate epsilon as max error using norm 2
+%  epsilon = max(||x_g - sysd.A*x_g - sysd.B*u_g||)
+
+N = size(x_g,1)/3;
+epsilon = 0;
+for i = 2:N-1
+    current_state = x_g(3*(i-1)+1:3*(i-1)+3);
+
+    predict = sysd.A*x_g(3*(i-2)+1:3*(i-2)+3) + sysd.B*u_g(i-1);
+    % error = x_g(i+1) - sysd.A*x_g(3*(i-2)+1:3*(i-2)+3) - sysd.B*u_g(i-1);
+    error = predict - current_state;
+    epsilon = max(norm(error, 2), epsilon);
+end
+
+
+% % 1. Calculate the error
+% error = x_g - sysd.A*x_g - sysd.B*u_g;
+%
+% % 2. Calculate the norm 2 of the error
+% epsilon = norm(error, 2);
+
+end
