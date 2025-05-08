54a0f554aaaff8eac2730a8e1c69c473d70f6c88
HadiElnemr <hadi.elnemr@gmail.com>
2025-05-08 22:41:25 +0200

    Add generate noise codes

---
+++ DDMPC Codes/bike_model_code_ACC/generate_noise.m
@@ -0,0 +1,15 @@
+% Set parameters
+nSteps = 150;
+% nStates = size(A_feedback, 1);
+
+% Model dimension
+n = 3;
+
+% Set noise characteristics
+epsilon = 10^-6;  % adjust to your needs
+
+% Generate Gaussian noise for each step
+% process_noise = process_noise_std * randn(nSteps, nStates);
+process_noise = -sqrt(epsilon)/sqrt(n)+2*sqrt(epsilon)/sqrt(n)*rand(n, nSteps);
+% Save to file
+save('process_noise.mat', 'process_noise');
