8dc17d38ff7284c4f694b35f349aac9589f21e35
HadiElnemr <hadi.elnemr@gmail.com>
2025-05-11 21:30:36 +0200

    modify generate noise

--- DDMPC Codes/bike_model_code_ACC/generate_noise.m
+++ DDMPC Codes/bike_model_code_ACC/generate_noise.m
@@ -12,4 +12,4 @@ epsilon = 10^-6;  % adjust to your needs
 % process_noise = process_noise_std * randn(nSteps, nStates);
 process_noise = -sqrt(epsilon)/sqrt(n)+2*sqrt(epsilon)/sqrt(n)*rand(n, nSteps);
 % Save to file
-save('process_noise.mat', 'process_noise');
+save('process_noise2.mat', 'process_noise');
